f794c9e04b8a63202842eb077c959284883044c8
Daniel Pichinil <daniel.pichinil@gmail.com>
2022-11-11 12:48:08 -0300

    creacion aplicativo
    se agrega los ficheros iniciales del aplicativo

---
+++ src/test/java/cl/dpichinil/test/springbootswagger/SpringBootSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.dpichinil.test.springbootswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
