4afb019673e9bb195d708f756a5e8407ecba4b01
Daniel Pichinil <daniel.pichinil@gmail.com>
2022-11-22 12:08:27 -0300

    Implementa swagger  para el proyecto
    Se implementa swagger para documentar la api del proyecto

--- src/main/java/cl/dpichinil/test/springbootswagger/SpringBootSwaggerApplication.java
+++ src/main/java/cl/dpichinil/test/springbootswagger/SpringBootSwaggerApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSwaggerApplication {
-
+    /**
+     * ejemplo en base a pagina
+     * https://www.baeldung.com/spring-rest-openapi-documentation
+     */
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSwaggerApplication.class, args);
     }
